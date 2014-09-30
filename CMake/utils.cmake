dbd920d0817deaf9f2ec12e72e0284e474455883
Yu Zhi <yuzhi@ipp.ac.cn>
2014-09-30 16:42:15 +0800

    snapshot
    Change-Id: I992ea22c131454b1ab2ea465df13e7a9fb207e0d
    Signed-off-by: Yu Zhi <yuzhi@ipp.ac.cn>

--- CMake/utils.cmake
+++ CMake/utils.cmake
@@ -1,7 +1,7 @@
   
 function(my_test name )
 
-  add_executable(${name} ${name}.cpp ${ARGN})
+ add_executable(${name} ${name}.cpp ${ARGN})
 
   if (BUILD_SHARED_LIBS)
     set_target_properties(${name}
@@ -9,9 +9,10 @@ function(my_test name )
       COMPILE_DEFINITIONS "GTEST_LINKED_AS_SHARED_LIBRARY=1 GTEST_HAS_TR1_TUPLE=0  ")
   endif()
   
-  target_link_libraries(${name} gtest_main gtest pthread)   
+target_link_libraries(${name} gtest_main gtest pthread)   
  
-  add_test(${name}  ${EXECUTABLE_OUTPUT_PATH}/${name}  )  
- 
-  ADD_DEPENDENCIES(${name} googletest) 
+#GTEST_ADD_TESTS(${name}  " "   ${name}.cpp    ${ARGN} )  
+ add_test(${name}  ${EXECUTABLE_OUTPUT_PATH}/${name}  )  
+
+ ADD_DEPENDENCIES(${name} googletest) 
 endfunction()
