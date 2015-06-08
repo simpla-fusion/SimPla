7dc60cc41a8d3f94f363c143222dad5a37acddf6
YU Zhi <yuzhi@ipp.ac.cn>
2015-06-08 15:39:22 +0800

    snapshoot
    Change-Id: Ie93b65922b9d9a3455646d70c83b213d313656e9
    Signed-off-by: YU Zhi <yuzhi@ipp.ac.cn>

--- example/model/demo_model.lua
+++ example/model/demo_model.lua
@@ -1,6 +1,6 @@
 
-NX = 128
-NY = 128 
+NX = 10
+NY = 10 
 NZ = 1
 LX = 10   --m --100000*rhoi --0.6
 LY = 10   --2.0*math.pi/k0
@@ -16,8 +16,9 @@ Mesh={
 Object=function(v)
    --res=( (v[1]-5.0)* (v[1]-5.0)+ (v[2]-5.0)* (v[2]-5.0))-10
    d1=( (v[1]-LX/2)* (v[1]- LX/2)+ (v[2]-LY/2)* (v[2]-LY/2))-LY*LY*0.04
-   d2=math.max( math.abs(v[1]-LX*0.6)-2 , math.abs(v[2]-LY*0.6)-2)
-   return math.min(d1,d2)
+--   d2=math.max( math.abs(v[1]-LX*0.6)-2.2 , math.abs(v[2]-LY*0.6)-2.2)
+--   return math.min(d1,d2)
+  return d1
  end
 SelectTag = 6
 
