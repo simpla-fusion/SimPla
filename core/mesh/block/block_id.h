9cacfbaaee6e5c13ecdb57c5a46e3bf654b41215
yuzhi <yuzhi@ipp.ac.cn>
2015-07-08 11:18:43 +0800

    snapshot

---
+++ core/mesh/block/block_id.h
@@ -0,0 +1,43 @@
+//
+// Created by salmon on 7/8/15.
+//
+
+#ifndef SIMPLA_BLOCK_ID_H
+#define SIMPLA_BLOCK_ID_H
+namespace simpla
+{
+
+struct BlockID
+{
+	typedef std::uint64_t value_type;
+private:
+	value_type m_id_;
+public:
+
+	BlockID(value_type const &v) : m_id_(v)
+	{
+	}
+
+	operator value_type() const
+	{
+		return m_id_;
+	}
+
+	value_type id() const { return m_id_; }
+
+
+	int level() const;
+
+	int rank() const;
+
+	int local_id() const;
+
+};
+
+struct BlockIDFactory
+{
+
+};
+}// namespace simpla
+
+#endif //SIMPLA_BLOCK_ID_H
