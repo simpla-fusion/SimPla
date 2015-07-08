384985c8816ae9493feabef96935b6fbb8d69947
yuzhi <yuzhi.student@gmail.com>
2015-07-09 04:15:23 +0800

    snapshot

---
+++ core/gtl/design_pattern/strategy.h
@@ -0,0 +1,10 @@
+//
+// Created by salmon on 7/9/15.
+//
+
+#ifndef SIMPLA_GTL_DESIGN_PATTERN_STRATEGY_H
+#define SIMPLA_GTL_DESIGN_PATTERN_STRATEGY_H
+namespace simpla {
+
+}// namespace simpla
+#endif //SIMPLA_GTL_DESIGN_PATTERN_STRATEGY_H
