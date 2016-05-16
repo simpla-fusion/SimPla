304e49ad06997c01fdd8ac2dc57dc609902f1386
yuzhi <yuzhi.student@gmail.com>
2016-05-16 20:53:51 +0800

    add opencascade,moab

---
+++ example/modeling/MakeBottle.cxx
@@ -0,0 +1,241 @@
+#include <QWidget>
+#include <QApplication>
+
+#include <BRep_Tool.hxx>
+
+#include <BRepAlgoAPI_Fuse.hxx>
+
+#include <BRepBuilderAPI_MakeEdge.hxx>
+#include <BRepBuilderAPI_MakeFace.hxx>
+#include <BRepBuilderAPI_MakeWire.hxx>
+#include <BRepBuilderAPI_Transform.hxx>
+
+#include <BRepFilletAPI_MakeFillet.hxx>
+
+#include <BRepLib.hxx>
+
+#include <BRepOffsetAPI_MakeThickSolid.hxx>
+#include <BRepOffsetAPI_ThruSections.hxx>
+
+#include <BRepPrimAPI_MakeCylinder.hxx>
+#include <BRepPrimAPI_MakePrism.hxx>
+
+#include <GC_MakeArcOfCircle.hxx>
+#include <GC_MakeSegment.hxx>
+
+#include <GCE2d_MakeSegment.hxx>
+
+#include <gp.hxx>
+#include <gp_Ax1.hxx>
+#include <gp_Ax2.hxx>
+#include <gp_Ax2d.hxx>
+#include <gp_Dir.hxx>
+#include <gp_Dir2d.hxx>
+#include <gp_Pnt.hxx>
+#include <gp_Pnt2d.hxx>
+#include <gp_Trsf.hxx>
+#include <gp_Vec.hxx>
+
+#include <Geom_CylindricalSurface.hxx>
+#include <Geom_Plane.hxx>
+#include <Geom_Surface.hxx>
+#include <Geom_TrimmedCurve.hxx>
+
+#include <Geom2d_Ellipse.hxx>
+#include <Geom2d_TrimmedCurve.hxx>
+
+#include <TopExp_Explorer.hxx>
+
+#include <TopoDS.hxx>
+#include <TopoDS_Edge.hxx>
+#include <TopoDS_Face.hxx>
+#include <TopoDS_Wire.hxx>
+#include <TopoDS_Shape.hxx>
+#include <TopoDS_Compound.hxx>
+
+#include <TopTools_ListOfShape.hxx>
+
+#include <TopoDS_Shape.hxx>
+#include <BRepPrimAPI_MakeSphere.hxx>
+#include <TDocStd_Document.hxx>
+#include <Handle_TDocStd_Document.hxx>
+#include <XCAFApp_Application.hxx>
+#include <Handle_XCAFApp_Application.hxx>
+#include <XCAFDoc_ShapeTool.hxx>
+#include <Handle_XCAFDoc_ShapeTool.hxx>
+#include <XCAFDoc_DocumentTool.hxx>
+#include <STEPCAFControl_Writer.hxx>
+#include <STEPCAFControl_Writer.hxx>
+#include <XCAFDoc_DocumentTool.hxx>
+#include <XCAFApp_Application.hxx>
+#include <GeomAPI_PointsToBSpline.hxx>
+#include <TColgp_Array1OfPnt.hxx>
+#include <Geom_BSplineCurve.hxx>
+#include <GeomAPI_Interpolate.hxx>
+#include <TColgp_HArray1OfPnt.hxx>
+
+#include <AIS.hxx>
+#include <AIS_Shape.hxx>
+#include <AIS_InteractiveContext.hxx>
+#include <Graphic3d_AspectLine3d.hxx>
+#include <Graphic3d_AspectMarker3d.hxx>
+#include <Graphic3d_AspectFillArea3d.hxx>
+#include <Graphic3d_AspectText3d.hxx>
+#include <Graphic3d_GraphicDriver.hxx>
+#include <OpenGl_GraphicDriver.hxx>
+#include <V3d.hxx>
+#include <V3d_View.hxx>
+#include <V3d_Viewer.hxx>
+
+
+#include <BRepPrimAPI_MakeBox.hxx>
+#include <Xw_Window.hxx>
+
+
+TopoDS_Shape
+MakeBottle(const Standard_Real myWidth, const Standard_Real myHeight,
+           const Standard_Real myThickness) {
+
+    BRepBuilderAPI_MakeWire wireMaker;
+
+    // Profile : Define Support Points
+    {
+        Handle(TColgp_HArray1OfPnt) gp_array = new TColgp_HArray1OfPnt(1, 4);
+        gp_Pnt p0(-myWidth / 2., 0, 0);
+        gp_Pnt p4(myWidth / 2., 0, 0);
+
+//    gp_array->SetValue(0, p0);
+        gp_array->SetValue(1, gp_Pnt(-myWidth / 2., -myThickness / 4., 0));
+        gp_array->SetValue(2, gp_Pnt(0, -myThickness / 2., 0));
+        gp_array->SetValue(3, gp_Pnt(myWidth / 2., -myThickness / 4., 0));
+        gp_array->SetValue(4, p4);
+
+        GeomAPI_Interpolate sp(gp_array, true, 1.0e-3);
+        sp.Perform();
+
+        wireMaker.Add(BRepBuilderAPI_MakeEdge(sp.Curve()));
+    }
+    // Body : Prism the Profile
+    TopoDS_Face myFaceProfile = BRepBuilderAPI_MakeFace(wireMaker.Wire());
+    gp_Vec aPrismVec(0, 0, myHeight);
+    TopoDS_Shape myBody = BRepPrimAPI_MakePrism(myFaceProfile, aPrismVec);
+
+
+
+    // Building the Resulting Compound
+    TopoDS_Compound aRes;
+    BRep_Builder aBuilder;
+    aBuilder.MakeCompound(aRes);
+    aBuilder.Add(aRes, myBody);
+//    aBuilder.Add(aRes, myThreading);
+
+    return aRes;
+}
+
+int main(int argc, char **argv) {
+
+    TopoDS_Shape shape = MakeBottle(50, 70, 30);
+
+
+    Handle(Aspect_DisplayConnection) aDisplayConnection = new Aspect_DisplayConnection();
+
+    Handle(OpenGl_GraphicDriver) aGraphicDriver = new OpenGl_GraphicDriver(aDisplayConnection);
+
+
+    Handle(V3d_Viewer) aViewer = new V3d_Viewer(aGraphicDriver, Standard_ExtString("Test"), "");
+//    ,
+//            // Space size
+//                                                400.,
+//            // Default projection
+//                                                V3d_Xpos,
+//            // Default  background
+//                                                Quantity_NOC_DARKVIOLET,
+//            // Type of  visualization
+//                                                V3d_ZBUFFER,
+//            // Shading  model
+//                                                V3d_GOURAUD,
+//            // Update mode
+//                                                V3d_WAIT
+//    );
+
+// set parameters for V3d_Viewer
+// defines default lights -
+//   positional-light 0.3 0.0 0.0
+//   directional-light V3d_XnegYposZpos
+//   directional-light V3d_XnegYneg
+//   ambient-light
+//    aViewer->SetDefaultLights();
+//// activates all the lights defined in this viewer
+//    aViewer->SetLightOn();
+//// set background color to black
+//    aViewer->SetDefaultBackgroundColor(Quantity_NOC_BLACK);
+
+
+    Handle(AIS_InteractiveContext) aContext = new AIS_InteractiveContext(aViewer);
+
+    Handle(AIS_Shape) anAis = new AIS_Shape(shape);
+
+    aContext->Display(anAis);
+
+
+    QApplication qApplication(argc, argv);
+
+    QWidget qWidget;
+
+    qWidget.show();
+
+    Handle(Xw_Window) aWindow = new Xw_Window(aDisplayConnection, qWidget.winId());
+
+    Handle(V3d_View) aView = aViewer->CreateView();
+
+    aView->SetWindow(aWindow);
+
+
+
+    /////////////////////////////////////////////////////////
+
+//    IVtkOCC_Shape::Handle aShapeImpl = new IVtkOCC_Shape(aShape);
+//
+//    vtkSmartPointer <IVtkTools_ShapeDataSource> DS = vtkSmartPointer<IVtkTools_ShapeDataSource>::New();
+//    DS->SetShape(aShapeImpl);
+//    IVtkOCC_Shape::Handle occShape = DS->GetShape();
+//    IVtkTools::InitShapeMapper(Mapper);
+//    vtkLookupTable *Table = IVtkTools::InitLookupTable();
+//    IVtkTools::InitShapeMapper(Mapper, Table);
+//    SetLookupTableColor(aLookupTable, MT_FreeEdge, R, G, B);
+//    SetLookupTableColor(aLookupTable, MT_FreeEdge, R, G, B, A);
+//    GetLookupTableColor(aLookupTable, MT_FreeEdge, R, G, B);
+//    GetLookupTableColor(aLookupTable, MT_FreeEdge, R, G, B, A);
+//    Mapper->ScalarVisibilityOn();  // use colors from lookup table
+//    Mapper->ScalarVisibilityOff(); // use a color of actor’s property
+//    vtkSmartPointer <IVtkTools_ShapeDataSource> DS = vtkSmartPointer<IVtkTools_ShapeDataSource>::New();
+//    vtkSmartPointer <IVtkTools_DisplayModeFilter> DMFilter = vtkSmartPointer<IVtkTools_DisplayModeFilter>::New();
+//    DMFilter->AddInputConnection(DS->GetOutputPort());
+//    DMFilter->SetDisplayMode(DM_Shading);
+//    vtkSmartPointer <vtkDataSetMapper> M = vtkSmartPointer<vtkDataSetMapper>::New();
+//    M->SetInputConnection(DMFilter->GetOutputPort());
+//    vtkSmartPointer <IVtkTools_ShapeDataSource> DS = vtkSmartPointer<IVtkTools_ShapeDataSource>::New();
+//    vtkSmartPointer <IVtkTools_DisplayModeFilter> DMFilter = vtkSmartPointer<IVtkTools_DisplayModeFilter>::New();
+//    DMFilter->AddInputConnection(DS->GetOutputPort());
+//    DMFilter->SetDisplayMode(DM_Shading);
+//    vtkSmartPointer <vtkDataSetMapper> M = vtkSmartPointer<vtkDataSetMapper>::New();
+//    M->SetInputConnection(DMFilter->GetOutputPort());
+//    // Create document
+//    Handle(TDocStd_Document) aDoc;
+//    Handle(XCAFApp_Application) anApp = XCAFApp_Application::GetApplication();
+//    anApp->NewDocument("MDTV-XCAF", aDoc);
+//
+//    // Create label and add our shape
+//    Handle (XCAFDoc_ShapeTool) myShapeTool =
+//            XCAFDoc_DocumentTool::ShapeTool(aDoc->Main());
+//    TDF_Label aLabel = myShapeTool->NewShape();
+//    myShapeTool->SetShape(aLabel, shape);
+//
+//    // Write as STEP file
+//    STEPCAFControl_Writer *myWriter = new STEPCAFControl_Writer();
+//    myWriter->Perform(aDoc, "demo.stp");
+
+
+    return qApplication.exec();
+
+}
---
+++ example/modeling/example_scdmesh.cpp
@@ -0,0 +1,102 @@
+/*!
+\example example_scdmesh.cpp
+
+\section example_scdmesh_cpp_title SCDMesh Example
+\subsection example_scdmesh_cpp_src Source Code
+*/
+
+#include "meshkit/MKCore.hpp"
+#include "meshkit/SCDMesh.hpp"
+#include "meshkit/SizingFunction.hpp"
+#include "meshkit/ModelEnt.hpp"
+#include "meshkit/VertexMesher.hpp"
+#include "meshkit/EdgeMesher.hpp"
+#include <vector>
+
+using namespace MeshKit;
+
+
+
+#ifdef HAVE_ACIS
+#define DEFAULT_TEST_FILE_1 "holycyl.sat"
+#define DEFAULT_TEST_FILE_2 "three_bricks.sat"
+#elif defined(HAVE_OCC)
+#define DEFAULT_TEST_FILE_1 "holycyl.stp"
+#define DEFAULT_TEST_FILE_2 "three_bricks.stp"
+#endif
+
+MKCore *mk;
+
+//---------------------------------------------------------------------------//
+// main function
+int main(int argc, char **argv)
+{
+  mk = new MKCore();
+
+  //---------------------------------------------------------------------------//
+  // *: Full mesh representation
+  // *: Cartesian bounding box
+  // *: Coarse/fine grid sizing
+  // *: All volumes meshed with a single grid
+  // load the test geometry
+
+  std::string scd_geom = (std::string) MESH_DIR + "/" + DEFAULT_TEST_FILE_1;
+  mk->load_geometry(scd_geom.c_str());
+
+  // get the volumes
+  MEntVector vols;
+  mk->get_entities_by_dimension(3, vols);
+
+  // make an SCD mesh instance with all volumes as separate model entities
+  SCDMesh *scdmesh = (SCDMesh*) mk->construct_meshop("SCDMesh", vols);
+
+  // provide the SCD mesh parameters for a cartesian grid
+  scdmesh->set_interface_scheme(SCDMesh::full);
+  scdmesh->set_grid_scheme(SCDMesh::cfMesh);
+  scdmesh->set_axis_scheme(SCDMesh::cartesian);
+  scdmesh->set_geometry_scheme(SCDMesh::all);
+
+  // i direction parameters
+  int ci_size = 5;
+  std::vector<int> fine_i(ci_size);
+  fine_i[0] = 2;
+  fine_i[1] = 2;
+  fine_i[2] = 10;
+  fine_i[3] = 2;
+  fine_i[4] = 2;
+  scdmesh->set_coarse_i_grid(ci_size);
+  scdmesh->set_fine_i_grid(fine_i);
+
+  // j direction parameters
+  int cj_size = 5;
+  std::vector<int> fine_j(cj_size);
+  fine_j[0] = 2;
+  fine_j[1] = 2;
+  fine_j[2] = 10;
+  fine_j[3] = 2;
+  fine_j[4] = 2;
+  scdmesh->set_coarse_j_grid(cj_size);
+  scdmesh->set_fine_j_grid(fine_j);
+
+  // k direction parameters
+  int ck_size = 5;
+  std::vector<int> fine_k(ck_size);
+  fine_k[0] = 2;
+  fine_k[1] = 2;
+  fine_k[2] = 10;
+  fine_k[3] = 2;
+  fine_k[4] = 2;
+  scdmesh->set_coarse_k_grid(ck_size);
+  scdmesh->set_fine_k_grid(fine_k);
+
+  // execute and create the structured grid
+  mk->setup_and_execute();
+
+  // write the mesh to a file
+  mk->save_mesh("SCDmesh1.vtk");
+
+  // free memory
+  delete scdmesh;
+  delete mk;
+  return 0;
+}
