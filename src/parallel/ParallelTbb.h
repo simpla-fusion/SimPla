aaf9fdc571c7bc3b8d1b8bf79fe76067cabeaa4b
yuzhi <yuzhi@ipp.ac.cn>
2016-09-27 14:02:51 +0800

    add include in ParallelTbb.h

--- src/parallel/ParallelTbb.h
+++ src/parallel/ParallelTbb.h
@@ -11,7 +11,9 @@
 //#define TBB_IMPLEMENT_CPP0X true
 
 #include <tbb/tbb.h>
-
+#include <tbb/concurrent_unordered_set.h>
+#include <tbb/concurrent_unordered_map.h>
+#include <tbb/concurrent_hash_map.h>
 namespace simpla { namespace parallel
 {
 using tbb::parallel_for;
