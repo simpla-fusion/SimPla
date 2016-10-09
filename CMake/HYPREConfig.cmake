b80a28544b17377f2be5547e18d505fa95fa1aac
Salmon <yuzhi@ipp.ac.cn>
2016-10-09 08:41:32 +0800

    SimPla: test cylindrical FDTD

--- CMake/HYPREConfig.cmake
+++ CMake/HYPREConfig.cmake
@@ -1,3 +1,30 @@
 
+
+
+
 SET(HYPRE_FOUND TRUE)
 SET(HYPRE_INCLUDE_DIRS /usr)
+
+SET(HYPRE_LIBRARIES
+        HYPRE_FEI_fgmres
+        HYPRE_DistributedMatrixPilutSolver
+        HYPRE_sstruct_mv
+        HYPRE_seq_mv
+        HYPRE_krylov
+        HYPRE_multivector
+        HYPRE_ParaSails
+        HYPRE_MatrixMatrix
+        HYPRE_parcsr_mv
+        HYPRE_utilities
+        HYPRE_FEI
+        HYPRE
+        HYPRE_parcsr_block_mv
+        HYPRE_struct_ls
+        HYPRE_Euclid
+        HYPRE_parcsr_ls
+        HYPRE_DistributedMatrix
+        HYPRE_mli
+        HYPRE_sstruct_ls
+        HYPRE_struct_mv
+        HYPRE_IJ_mv
+        )
---
+++ example/cylindrical/README.md
@@ -0,0 +1,20 @@
+
+Description:
+----------------------
+  - cylindrical coordinates
+  - Cold Plasma Fluid
+  - FDTD EM solver  
+
+Build:
+--------------
+    $tar xzvf <src>.tar.gz
+    $mkdir build
+    $cd build
+    $cmake ../<src dir>/ -DCMAKE_BUILD_TYPE=Release
+    $make demo_em
+
+Usage:
+----------------------
+    $./demo_em -i demo.lua
+    $python check_point.py ./simpla.h5  "/checkpoint/E"  2 -1&
+
