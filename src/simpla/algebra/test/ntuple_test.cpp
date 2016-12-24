0858b4e850ba84cce195fbab7e92f7415c3cd310
Salmon <yuzhi@ipp.ac.cn>
2016-12-24 09:45:16 +0800

    fixme: algebra::get_v(T[][][],size_type*)

--- src/simpla/algebra/test/ntuple_test.cpp
+++ src/simpla/algebra/test/ntuple_test.cpp
@@ -35,22 +35,22 @@ protected:
 
 //        DIMENSIONS = extents();
 
-//        traits::seq_for_each(extents(),
-//                             [&](size_t const *idx)
-//                             {
+        traits::seq_for_each(extents(),
+                             [&](size_t const *idx)
+                             {
 //                                 algebra::get_v(aA, idx) = 1;//static_cast<value_type>(idx[0] * 2);
 //                                 algebra::get_v(aB, idx) = static_cast<value_type>(5 - idx[0]);
 //                                 algebra::get_v(aC, idx) = static_cast<value_type>(idx[0] * 5 + 1);
 //                                 algebra::get_v(aD, idx) = static_cast<value_type>(0);
-//                                 algebra::get_v(vA, idx) = algebra::get_v(aA, idx);
-//                                 algebra::get_v(vB, idx) = algebra::get_v(aB, idx);
-//                                 algebra::get_v(vC, idx) = algebra::get_v(aC, idx);
-//                                 algebra::get_v(vD, idx) = static_cast<value_type>(0);
+                                 algebra::get_v(vA, idx) = algebra::get_v(aA, idx);
+                                 algebra::get_v(vB, idx) = algebra::get_v(aB, idx);
+                                 algebra::get_v(vC, idx) = algebra::get_v(aC, idx);
+                                 algebra::get_v(vD, idx) = static_cast<value_type>(0);
 //                                 algebra::get_v(res, idx) = -(algebra::get_v(aA, idx) + a) /
 //                                                            (algebra::get_v(aB, idx) * b - c) -
 //                                                            algebra::get_v(aC, idx);
-//
-//                             });
+
+                             });
 
         num_of_loops = 1000000L;
     }
@@ -122,15 +122,15 @@ TYPED_TEST(TestNtuple, assign_Scalar)
 TYPED_TEST(TestNtuple, assign_Array)
 {
 
-//    TestFixture::vA = TestFixture::aA;
-//
-//    traits::seq_for_each(typename TestFixture::extents(),
-//                         [&](size_type const *idx)
-//                         {
-//                             EXPECT_DOUBLE_EQ(0, abs(algebra::get_v(TestFixture::aA, idx) -
-//                                                     algebra::get_v(TestFixture::vA, idx)));
-//                         }
-//    );
+    TestFixture::vA = TestFixture::aA;
+
+    traits::seq_for_each(typename TestFixture::extents(),
+                         [&](size_type const *idx)
+                         {
+                             EXPECT_DOUBLE_EQ(0, abs(algebra::get_v(TestFixture::aA, idx) -
+                                                     algebra::get_v(TestFixture::vA, idx)));
+                         }
+    );
 
 
 }
