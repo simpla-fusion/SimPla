a692715b18a37e2f8fa64648fae55ca7430796cc
Salmon <yuzhi@ipp.ac.cn>
2017-01-03 10:01:52 +0800

    SimPla: Array

--- src/simpla/algebra/nTuple.h
+++ src/simpla/algebra/nTuple.h
@@ -263,8 +263,7 @@ inline void _apply(TOP const &, declare::nTuple_<TL, I0, I1> &lhs, TR &rhs)
     for (size_type i = 0; i < I0; ++i)
         for (size_type j = 0; j < I1; ++j)
         {
-            TOP::eval(calculator::get_value(lhs, i, j),
-                      calculator::get_value(rhs, i, j));
+            TOP::eval(calculator::get_value(lhs, i, j), calculator::get_value(rhs, i, j));
         }
 };
 
@@ -309,67 +308,55 @@ struct calculator<declare::nTuple_<V, J...>>
 public:
     template<typename T> static constexpr inline T &get_value(T &v) { return v; };
 
-//    template<typename T, typename I0>
-//    static constexpr inline st::remove_all_extents_t<T, I0> &
-//    get_value(T &v, I0 const *s, ENABLE_IF((st::is_indexable<T, I0>::value)))
-//    {
-//        return get_value(v[*s], s + 1);
-//    };
-//
-//    template<typename T, typename I0>
-//    static constexpr inline st::remove_all_extents_t<T, I0> &
-//    get_value(T &v, I0 const *s, ENABLE_IF((!st::is_indexable<T, I0>::value)))
-//    {
-//        return v;
-//    };
+    template<typename T, typename I0>
+    static constexpr inline st::remove_all_extents_t<T, I0> &
+    get_value(T &v, I0 const *s, ENABLE_IF((st::is_indexable<T, I0>::value)))
+    {
+        return get_value(v[*s], s + 1);
+    };
+
+    template<typename T, typename I0>
+    static constexpr inline st::remove_all_extents_t<T, I0> &
+    get_value(T &v, I0 const *s, ENABLE_IF((!st::is_indexable<T, I0>::value)))
+    {
+        return v;
+    };
 
 private:
     template<typename T, typename... Args>
-    static constexpr inline T &
-    get_value_(std::integral_constant<bool, false> const &, T &v, Args &&...) { return v; }
+    static constexpr inline auto & //T &
+    get_value_(std::integral_constant<bool, false> const &, T &v, Args &&...) AUTO_RETURN(v)
 
     template<typename T, typename I0, typename... Idx>
-    static constexpr inline st::remove_extents_t<T, I0, Idx...> &
-    get_value_(std::integral_constant<bool, true> const &, T &v, I0 const &s0, Idx &&... idx)
-    {
-        return get_value(v[s0], std::forward<Idx>(idx)...);
-    };
+    static constexpr inline auto &//st::remove_extents_t<T, I0, Idx...> &
+    get_value_(std::integral_constant<bool, true> const &, T &v, I0 const &s0, Idx &&... idx) AUTO_RETURN(
+            (get_value(v[s0], std::forward<Idx>(idx)...)))
+
 
 public:
     template<typename T, typename... Idx>
-    static constexpr inline auto // st::remove_extents_t<T, size_type, Idx...> &
-    get_value(T &v, size_type const &s0, Idx &&... idx)
-    {
-        return get_value_(std::integral_constant<bool, st::is_indexable<T, size_type>::value>(),
-                          v, s0, std::forward<Idx>(idx)...);
-    };
+    static constexpr inline auto &// st::remove_extents_t<T, size_type, Idx...> &
+    get_value(T &v, size_type const &s0, Idx &&... idx) AUTO_RETURN((get_value_(
+            std::integral_constant<bool, st::is_indexable<T, size_type>::value>(),
+            v, s0, std::forward<Idx>(idx)...)))
 
-//    template<typename T, size_type N>
-//    static constexpr inline T &get_value(declare::nTuple_<T, N> &v,
-//                                         size_type const &s0)
-//    {
-//        return v[s0];
-//    };
-//
-//    template<typename T, size_type N>
-//    static constexpr inline T const &get_value(declare::nTuple_<T, N> const &v,
-//                                               size_type const &s0)
-//    {
-//        return v[s0];
-//    };
+    template<typename T, size_type N>
+    static constexpr inline T &get_value(declare::nTuple_<T, N> &v, size_type const &s0) { return v[s0]; };
+
+    template<typename T, size_type N>
+    static constexpr inline T const &get_value(declare::nTuple_<T, N> const &v, size_type const &s0) { return v[s0]; };
 
 public:
     template<typename TOP, typename ...Others, size_type ... index, typename ...Idx>
     static inline auto //traits::value_type_t<declare::Expression<TOP, Others...>>
-    _invoke_helper(declare::Expression<TOP, Others...> const &expr, index_sequence<index...>, Idx &&... s)
-    AUTO_RETURN((expr.m_op_(get_value(std::get<index>(expr.m_args_), std::forward<Idx>(s)...)...)))
+    _invoke_helper(declare::Expression<TOP, Others...> const &expr, index_sequence<index...>, Idx &&... s) AUTO_RETURN(
+            (expr.m_op_(get_value(std::get<index>(expr.m_args_), std::forward<Idx>(s)...)...)))
 
 
     template<typename TOP, typename   ...Others, typename ...Idx>
     static constexpr inline auto //traits::value_type_t<declare::Expression<TOP, Others...> >
-    get_value(declare::Expression<TOP, Others...> const &expr, Idx &&... s)
-    AUTO_RETURN((_invoke_helper(expr, index_sequence_for<Others...>(), std::forward<Idx>(s)...)))
-
+    get_value(declare::Expression<TOP, Others...> const &expr, Idx &&... s) AUTO_RETURN(
+            (_invoke_helper(expr, index_sequence_for<Others...>(), std::forward<Idx>(s)...)))
 
 
     template<typename TOP, typename TR>
