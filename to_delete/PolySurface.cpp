9026a10d48ad82f3290e406dca7595c943a0e586
yuzhi <yuzhi@ipp.ac.cn>
2017-11-26 15:43:38 +0800

    remove to_delete:

--- to_delete/PolySurface.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by salmon on 17-10-23.
-//
-
-#include "PolySurface.h"
--- to_delete/PolySurface.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by salmon on 17-10-23.
-//
-
-#ifndef SIMPLA_POLYSURFACE_H
-#define SIMPLA_POLYSURFACE_H
-
-#endif //SIMPLA_POLYSURFACE_H
